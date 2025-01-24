36531ebd7c682728bd3c4407286a8fe55fd35d3d
juntaoJianggavin <gavinjaing97@gmail.com>
2025-01-24 21:45:32 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+from glob import glob
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+import yaml
+from albumentations.augmentations import transforms
+from albumentations.core.composition import Compose
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import archs as archs
+from dataset import Dataset
+from metrics import iou_score
+from utils import AverageMeter
+from albumentations import RandomRotate90,Resize
+import time
+import random, os
+import numpy as np
+from LV_UNet import LV_UNet
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--name', default=None,
+                        help='model name')
+    parser.add_argument('--deploy', default=False, type=str2bool)
+    args = parser.parse_args()
+
+    return args
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed()
+    np.random.seed(42)
+    random.seed(42)
+def main():
+    args = parse_args()
+
+    with open('models/%s/config.yml' % args.name, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    print('-'*20)
+    for key in config.keys():
+        print('%s: %s' % (key, str(config[key])))
+    print('-'*20)
+    model =LV_UNet()
+    model = model.cuda()
+
+    # Data loading code
+    img_ids = glob(os.path.join('inputs', config['dataset'], 'images', '*' + config['img_ext']))
+    img_ids = [os.path.splitext(os.path.basename(p))[0] for p in img_ids]
+    train_img_ids, test_img_ids = train_test_split(img_ids, test_size=0.8, random_state=42)
+    test_img_ids, val_img_ids = train_test_split(test_img_ids, test_size=0.2, random_state=42)
+
+    if args.deploy ==True:
+        model.switch_to_deploy()
+        model.load_state_dict(torch.load('models/%s/last_model.pth' %
+                                     config['name']))
+    else:
+        model.load_state_dict(torch.load('models/%s/best_model.pth' %
+                                     config['name']))
+    model.eval()
+    
+    val_transform = Compose([
+        Resize(config['input_h'], config['input_w']),
+        transforms.Normalize(),
+    ])
+    g = torch.Generator()
+    g.manual_seed(42)
+    val_dataset = Dataset(
+        img_ids=test_img_ids,
+        img_dir=os.path.join('inputs', config['dataset'], 'images'),
+        mask_dir=os.path.join('inputs', config['dataset'], 'masks'),
+        img_ext=config['img_ext'],
+        mask_ext=config['mask_ext'],
+        num_classes=config['num_classes'],
+        transform=val_transform)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=config['batch_size'],
+        shuffle=False,
+        num_workers=config['num_workers'],
+        worker_init_fn=seed_worker,
+        generator=g,
+        drop_last=False)
+
+    iou_avg_meter = AverageMeter()
+    dice_avg_meter = AverageMeter()
+    gput = AverageMeter()
+    cput = AverageMeter()
+
+    count = 0
+    for c in range(config['num_classes']):
+        os.makedirs(os.path.join('outputs', config['name'], str(c)), exist_ok=True)
+    with torch.no_grad():
+        for input, target, meta in tqdm(val_loader, total=len(val_loader)):
+            input = input.cuda()
+            target = target.cuda()
+            model = model.cuda()
+            # compute output
+            output = model(input)
+            iou,dice = iou_score(output, target)
+            iou_avg_meter.update(iou, input.size(0))
+            dice_avg_meter.update(dice, input.size(0))
+
+            output = torch.sigmoid(output).cpu().numpy()
+            output[output>=0.5]=1
+            output[output<0.5]=0
+
+            for i in range(len(output)):
+                cv2.imwrite(os.path.join('outputs', config['name'], str(c), meta['img_id'][i] + '.jpg'),
+                                (output[i, c] * 255).astype('uint8'))
+
+    print('IoU: %.4f' % iou_avg_meter.avg)
+    print('Dice: %.4f' % dice_avg_meter.avg)
+
+    torch.cuda.empty_cache()
+
+
+if __name__ == '__main__':
+    main()
